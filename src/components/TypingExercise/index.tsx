ed1dc315083d844f99cf42bb8f6a28ebef17533b
Juan <juanpa.box.77@gmail.com>
2024-06-18 13:21:52 -0300

    working in progress/ add animation when change letter

--- src/components/TypingExercise/index.tsx
+++ src/components/TypingExercise/index.tsx
@@ -1,15 +1,43 @@
 'use client'
 
+import { useEffect, useState } from 'react'
 import style from './index.module.css'
 import useCheckLetter from './useCheckLetter'
 const TypingExercise = ({ letters }: { letters: string[] }) => {
   const indexLetter = useCheckLetter(letters)
 
+  const [animate, setAnimate] = useState(true)
+  // console.log(animate)
+  useEffect(() => {
+    setAnimate(true)
+    console.log('animate')
+    return () => {
+      setTimeout(() => setAnimate(false), 800)
+      setAnimate(true)
+    }
+  }, [])
+
   return (
     <div className={style.container}>
-      <div>{letters[indexLetter]}</div>
+      <div className={style.letter}>{letters[indexLetter]}</div>
+      {/* <Letter letter={letters[indexLetter]} /> */}
     </div>
   )
+}
+
+const Letter = ({ letter }: { letter: string }) => {
+  const [animate, setAnimate] = useState(true)
+  // console.log(animate)
+  useEffect(() => {
+    setAnimate(true)
+    console.log('animate')
+    return () => {
+      setTimeout(() => setAnimate(false), 800)
+      setAnimate(true)
+    }
+  }, [letter])
+
+  return <div className={`${style.letter} ${animate ? style.animate : ''}`}>{letter}</div>
 }
 
 export default TypingExercise
