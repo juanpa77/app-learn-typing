1d488f3e329fdee351cf2f71337791a6d8d3dd17
Juan <juanpa.box.77@gmail.com>
2024-06-21 10:10:48 -0300

    add useWinnings - move addEventListener out of useEffect

--- src/components/TypingExercise/useCheckLetter.tsx
+++ src/components/TypingExercise/useCheckLetter.tsx
@@ -1,33 +1,35 @@
 import { useState, useEffect } from "react"
+import useWinnings from "./useWinnings"
 
 const useShowCharacter = (characters: string[]) => {
   const totalLetters = characters.length
-  const formatLetters = characters.map(character => character.toLowerCase())
+  const formatCharacter = characters.map(character => character.toLowerCase())
 
   const [indexCharacter, setIndexCharacter] = useState(0)
   const [keyPress, setKeyPress] = useState('')
   const [animate, setAnimate] = useState(false)
 
   const handleOnKeyDown = (e: KeyboardEvent) => setKeyPress(e.key)
-  const isCorrectKeyPress = formatLetters[indexCharacter] === keyPress
+  const isCorrectKeyPress = formatCharacter[indexCharacter] === keyPress
 
   const currentCharacter = characters[indexCharacter]
+  const winnings = useWinnings(keyPress, formatCharacter[indexCharacter])
+  document.addEventListener('keydown', handleOnKeyDown)
 
   useEffect(() => {
-    document.addEventListener('keydown', handleOnKeyDown)
     isCorrectKeyPress
       ? setIndexCharacter(indexCharacter + 1)
       : ''
 
     setAnimate(false)
-    setTimeout(() => setAnimate(true), 800)
+    setTimeout(() => setAnimate(true), 250)
 
     return () => {
       document.removeEventListener('keydown', handleOnKeyDown)
     }
-  }, [indexCharacter, isCorrectKeyPress])
+  }, [isCorrectKeyPress])
 
-  return { currentCharacter, animate }
+  return { currentCharacter, animate, winnings }
 }
 
 export default useShowCharacter
