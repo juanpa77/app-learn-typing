06eae0140e74448a49ab4dc6bef1f9b0358d662e
Juan <juanpa.box.77@gmail.com>
2024-06-24 06:22:37 -0300

    working in progress/ refactoring/ split hook

--- src/components/TypingExercise/useCheckLetter.tsx
+++ src/components/TypingExercise/useCheckLetter.tsx
@@ -9,27 +9,49 @@ const useShowCharacter = (characters: string[]) => {
   const [keyPress, setKeyPress] = useState('')
   const [animate, setAnimate] = useState(false)
 
-  const handleOnKeyDown = (e: KeyboardEvent) => setKeyPress(e.key)
+  // const handleOnKeyDown = (e: KeyboardEvent) => setKeyPress(e.key)
   const isCorrectKeyPress = formatCharacter[indexCharacter] === keyPress
 
   const currentCharacter = characters[indexCharacter]
   const winnings = useWinnings(keyPress, formatCharacter[indexCharacter])
-  document.addEventListener('keydown', handleOnKeyDown)
+  // document.addEventListener('keydown', handleOnKeyDown)
 
   useEffect(() => {
-    isCorrectKeyPress
-      ? setIndexCharacter(indexCharacter + 1)
-      : ''
+    if (isCorrectKeyPress) {
+      console.log(keyPress)
+      setIndexCharacter(indexCharacter + 1)
+      indexCharacter === totalLetters - 1 && setIndexCharacter(0)
+    }
 
     setAnimate(false)
     setTimeout(() => setAnimate(true), 250)
 
     return () => {
-      document.removeEventListener('keydown', handleOnKeyDown)
+      // document.removeEventListener('keydown', handleOnKeyDown)
     }
   }, [isCorrectKeyPress])
 
-  return { currentCharacter, animate, winnings }
+  return { currentCharacter, animate, winnings, keyPress, setKeyPress }
+}
+export const useKey = (onKeyPress: (key: string) => void) => {
+
+  const [keyPress, setKeyPress] = useState('')
+
+  const handleOnKeyDown = (e: KeyboardEvent) => {
+    onKeyPress(e.key)
+    setKeyPress(e.key)
+  }
+
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleOnKeyDown)
+
+    return () => {
+      document.removeEventListener('keydown', handleOnKeyDown)
+    }
+  }, [])
+
+  return keyPress
 }
 
 export default useShowCharacter
